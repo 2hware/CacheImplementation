5b3d4312a4ab6266c363b2ad3b66bf18d859f570
Hussein Hakak <Hussein Hakak>
2016-07-11 17:58:37 +1000

    Initial commit

---
+++ src/com/macquarie/examples/TestOne.java
@@ -0,0 +1,114 @@
+        package com.macquarie.examples;
+
+
+        import java.util.*;
+
+        public class TestOne {
+
+            public Map<CacheKey, List<CachedItem>> getCache() {
+                return cache;
+            }
+
+            private Map<CacheKey, List<CachedItem>> cache
+            = new HashMap<CacheKey, List<CachedItem>>();
+
+    public void addToCache(CachedItem item) {
+        CacheKey key = new CacheKey(item);
+        if (!cache.containsKey(key)) {
+            cache.put(key, new ArrayList<CachedItem>());
+            cache.get(key).add(item);
+        }else {
+
+            if (!cache.get(key).contains(item)) {
+                cache.get(key).add(item);
+            }
+        }
+
+
+    }
+
+    public void removeFromCache(CachedItem item) {
+        CacheKey key = new CacheKey(item);
+        if (cache.containsKey(key)) {
+            cache.get(key).remove(item);
+        }
+    }
+
+     static class CacheKey {
+        private final Integer intProperty;
+        private final String strOne;
+        private int hash;
+        public CacheKey(CachedItem item) {
+            this.intProperty = item.getIntProperty();
+            this.strOne = item.getStrOne();
+            this.hash=item.getStrOne().hashCode();
+        }
+        public Integer getIntProperty() {
+            return intProperty;
+        }
+        public String getStrOne() {
+            return strOne;
+        }
+
+        @Override
+        public boolean equals(Object anObject) {
+            if (this == anObject) {
+                return true;
+            }
+            if (anObject instanceof CacheKey) {
+                CacheKey anotherCacheKey = (CacheKey)anObject;
+                if((this.getIntProperty()==anotherCacheKey.getIntProperty()) && (this.getStrOne().equals(anotherCacheKey.getStrOne())))
+                      return true;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode(){
+            return this.intProperty+this.hash;
+        }
+
+
+    }
+
+      static class CachedItem {
+        private Integer intProperty;
+        private String strOne;
+        private String strTwo;
+        public CachedItem(Integer intProperty, String strOne, String strTwo) {
+            this.intProperty = intProperty;
+            this.strOne = strOne;
+            this.strTwo = strTwo;
+        }
+        public Integer getIntProperty() {
+            return intProperty;
+        }
+        public String getStrOne() {
+            return strOne;
+        }
+        public String getStrTwo() {
+            return strTwo;
+        }
+
+        @Override
+        public boolean equals(Object anObject) {
+            if (this == anObject) {
+                return true;
+            }
+            if (anObject instanceof CachedItem) {
+                CachedItem anotherCachedItem = (CachedItem)anObject;
+                if((this.getIntProperty()==anotherCachedItem.getIntProperty()) && (this.getStrOne().equals(anotherCachedItem.getStrOne()) && (this.getStrTwo().equals(anotherCachedItem.getStrTwo()))))
+                    return true;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode(){
+            return this.intProperty+this.getStrOne().hashCode()+this.getStrTwo().hashCode();
+        }
+
+    }
+
+
+}
---
+++ src/com/macquarie/examples/TestOneTest.java
@@ -0,0 +1,77 @@
+package com.macquarie.examples;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import  com.macquarie.examples.TestOne.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Hussein on 2016-06-30.
+ */
+public class TestOneTest {
+
+
+    @Test
+    public void test_should_not_duplicate_keys_in_cache(){
+
+        TestOne testOne=new TestOne();
+        CachedItem  cachedItem1 = new CachedItem(1,"one","some test");
+        CachedItem  cachedItem2 = new CachedItem(1,"one","extra text");
+        CachedItem  cachedItem3 = new CachedItem(1,"134","extra text");
+        testOne.addToCache(cachedItem1);
+        testOne.addToCache(cachedItem2);
+        testOne.addToCache(cachedItem3);
+        testOne.addToCache(cachedItem3);
+
+        assertEquals(2,testOne.getCache().size());
+
+    }
+
+    @Test
+    public void test_should_not_duplicate_cached_Item_under_same_key(){
+
+        TestOne testOne=new TestOne();
+        TestOne.CachedItem  cachedItem1 = new TestOne.CachedItem(1,"one","some test");
+        TestOne.CachedItem  cachedItem2 = new TestOne.CachedItem(1,"one","extra text");
+        TestOne.CachedItem  cachedItem3 = new TestOne.CachedItem(2,"two","extra text");
+        TestOne.CachedItem  cachedItem4 = new TestOne.CachedItem(2,"two","extra text");
+        testOne.addToCache(cachedItem1);
+        testOne.addToCache(cachedItem2);
+        testOne.addToCache(cachedItem3);
+        testOne.addToCache(cachedItem4);
+        assertEquals(2,testOne.getCache().size());
+        TestOne.CacheKey cacheKey=new TestOne.CacheKey(cachedItem3);
+        Optional<TestOne.CacheKey> cacheKeyOptional=testOne.getCache().keySet().stream().filter(key -> key.equals(cacheKey)).findFirst();
+        assertEquals(1,testOne.getCache().get(cacheKeyOptional.get()).size());
+
+    }
+
+
+
+    @Test
+    public void test_should_remove_cached_Item_under_same_key(){
+
+        TestOne testOne=new TestOne();
+        TestOne.CachedItem  cachedItem1 = new TestOne.CachedItem(1,"one","some test");
+        TestOne.CachedItem  cachedItem2 = new TestOne.CachedItem(1,"one","extra text");
+        TestOne.CachedItem  cachedItem3 = new TestOne.CachedItem(2,"two","extra text");
+        TestOne.CachedItem  cachedItem4 = new TestOne.CachedItem(2,"two","extra text");
+        testOne.addToCache(cachedItem1);
+        testOne.addToCache(cachedItem2);
+        testOne.addToCache(cachedItem3);
+        testOne.addToCache(cachedItem4);
+        TestOne.CacheKey cacheKey=new TestOne.CacheKey(cachedItem1);
+        Optional<TestOne.CacheKey> cacheKeyOptional=testOne.getCache().keySet().stream().filter(key -> cacheKey.equals(key)).findFirst();
+
+        assertEquals(2,testOne.getCache().get(cacheKeyOptional.get()).size());
+
+        testOne.removeFromCache(cachedItem1);
+        assertEquals(1,testOne.getCache().get(cacheKeyOptional.get()).size());
+
+    }
+
+}
